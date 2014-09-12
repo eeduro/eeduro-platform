1f9375c3e3598be8c86a69fe01da98b3d93b73d8
Adam Bajric <adam.bajric@ntb.ch>
2014-09-12 12:31:51 +0200

    force should not be manipulated directly

--- delta/control/Inertia.cpp
+++ delta/control/Inertia.cpp
@@ -20,6 +20,8 @@ void Inertia::run() {
 	
 	q012 = jointPosIn.getSignal().getValue().getSubMatrix<3,1>(0, 0);
 	xyz = tcpPosIn.getSignal().getValue().getSubMatrix<3,1>(0, 0);
+	Vector3 a = accelerationIn.getSignal().getValue().getSubMatrix<3,1>(0, 0);
+	double qdd = accelerationIn.getSignal().getValue()[3];
 	
 	if (jacobi.calculate(q012, xyz)) {
 		Matrix<3,3> jacobian = jacobi.getJacobian();
@@ -27,28 +29,15 @@ void Inertia::run() {
 			Matrix<3,3> inverseJacobian = !jacobian;
 			Matrix<3,3> inverseJacobianTransposed = inverseJacobian.transpose();
 
-			Matrix<3,3> M = tcpMass + inverseJacobianTransposed * motorInertia * inverseJacobian;
-			Vector3 F = M * accelerationIn.getSignal().getValue().getSubMatrix<3,1>(0, 0);
-			
-			// Gravitation kompensieren: konstante Kraft nach oben
-// 			F[2] = 9.81 * mtcp;
-			
-			// max. Kraft in x-Richtung ist 1 N
-// 			double Fx = F[0];
-// 			if (F[0] < 0) Fx = -Fx;
-// 			
-// 			if (Fx > 1)
-// 				Fx = 1;
-// 			
-// 			F[0] = (F[0] < 0) ? -Fx : Fx;
-			
-			// Feder:
-// 			F[2] = 0;
-// 			double z_min = -0.04;
-// 			if (xyz[2] < z_min)
-// 				F[2] = 500 * (z_min - xyz[2]);
+// 			a[0] = 0; // Regelung in x-Richtung ausschalten
+// 			a[1] = 0; // Regelung in y-Richtung ausschalten
+// 			a[2] = 0; // Regelung in z-Richtung ausschalten
 
-			forceOut.getSignal().setValue(Vector4(F[0], F[1], F[2], jred * accelerationIn.getSignal().getValue()[3]));
+			Matrix<3,3> M = tcpMass + inverseJacobianTransposed * motorInertia * inverseJacobian;
+			Vector3 F = M * a;
+			double tau = jred * qdd; // magnet axis
+
+			forceOut.getSignal().setValue(Vector4(F[0], F[1], F[2], tau));
 			forceOut.getSignal().setTimestamp(accelerationIn.getSignal().getTimestamp());
 			
 			return;
